f6f39bff85cc63675e4a9dd0bb4d8c712ec500ef
Siddhant <santoshshrivastava39@gmail.com>
2022-07-31 17:36:49 +0530

    Creating Address Book

---
+++ AddressBook.js
@@ -0,0 +1,29 @@
+
+//UC1 Ability to create a Address Book Contact with first and last names,address, city, state, zip, phone number and email
+class Contact{
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    eMail;
+
+    constructor(...params){
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNumber = params[6];
+        this.eMail = params[7];
+    }
+
+        toString(){
+            return ("First Name: "+this.firstName+" \nLast Name: "+this.lastName+"\nAddress: "+this.address+"\nCity : "+this.city+"\nState: "+this.state+"\nZip Code : "+this.zip+"\nPhone: "+this.phoneNumber+ "\nE-Mail: "+this.eMail);
+        }
+}
+let contactObj = new Contact("Siddhant","Shrivastava","XYZ","Ujjain","MP",453432,8789876653,"siddhant@gmail.com");
+console.log(contactObj.toString());
